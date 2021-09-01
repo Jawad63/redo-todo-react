ba97535f89901288cda15146229cc3f3314b06ad
Jawad63 <jawadqureshi111@gmail.com>
2021-09-01 15:40:38 +0200

    creating input field and buttons

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import TodoList from "./TodoList";
 
 function App() {
   return (
-    // creating a todo list: 
-    <TodoList />
+    // adding the input for new Todo:
+    // A fragment(<></>) will allow us to return multiple things. A JS function will only return one thing that's why we have to use an empty element here.
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Delete Todo</button>
+    </>
       
   )
 }
