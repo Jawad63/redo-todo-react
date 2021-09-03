dd06116a9c375950375031981b6e47dd2950317e
Jawad63 <jawadqureshi111@gmail.com>
2021-09-03 13:19:35 +0200

    todoNameRef works !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // need to store all of the changes/todos inside of a state so we can render those todos and everytime we add a todo, change a todo or delete a todo it will render the entire component tree for us. For that we will use: useState hook.
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // connecting app with TodoList.js:
+// To get access to the name inside field input we'll use UseRef Hook: It will allows us to reference elements inside our HTML. for us the input field.
 import TodoList from "./TodoList";
 
 
@@ -16,14 +17,31 @@ function App() {
                     
   const [todos, setTodos] = useState([]);
 
+  //creating a variable for name reference:
+  const todoNameRef = useRef()
+
+  // creating handleAddTodo function:
+  // e = event property.
+  function handleAddTodo(e) {
+    // here we need to set our Todos to one more Todo, take the previous todo add the new one and set them to our TODO list: 
+    // .current allows us to reference any element that we are currently refereing to in our case the input:
+    // .value will give us the value of our input:
+
+    const name = todoNameRef.current.value
+    if (name === '') return
+      console.log(name)
+
+  }
+
 
   return (
     // adding the input for new Todo:
     // A fragment(<></>) will allow us to return multiple things. A JS function will only return one thing that's why we have to use an empty element here.
     <>
       <TodoList todos = {todos} />
-      <input type="text" />
-      <button className="btn btn-primary mx-2">Add Todo</button>
+      <input type="text" ref={todoNameRef} />
+      {/* creating an onclick so when we can add our todos to the list */}
+      <button className="btn btn-primary mx-2" onClick={handleAddTodo}>Add Todo</button>
       <button className="btn btn-danger mx-2">Delete Todo</button>
       <h3 className="mx-3">How many left to do?</h3>
     </>
